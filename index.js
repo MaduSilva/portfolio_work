c727dd72e0609fbc8cdf289011181a55fb47fb5b
Maria Eduarda <maria.dsilva@amedigital.com>
2022-09-01 14:20:31 -0300

    work finished

---
+++ index.js
@@ -0,0 +1,10 @@
+//função que chama um alert avisando ao usuário que a mensagem foi enviada
+function Message() {
+  alert("Mensagem enviada com sucesso! Irei retornar em breve :)");
+}
+
+//função que limpa todos os inputs e textareas, substituindo por vazio ""
+function Clean() {
+  $("input").val("");
+  $("textarea").val("");
+}
